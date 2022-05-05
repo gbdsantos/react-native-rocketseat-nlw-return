a62ec8d45ca03f53238d189a71f602b6e38122e7
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-05-05 20:50:09 -0300

    :gem: feat(mobile): create button component

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import {
-  View
+  TouchableOpacity,
 } from 'react-native';
 
+import { ChatTeardropDots } from 'phosphor-react-native';
+
 import { styles } from './styles';
+import { theme } from '../../theme';
 
 export function Widget(){
   return (
-    <View>
-
-    </View>
+    <>
+      <TouchableOpacity 
+        style={styles.button}
+      >
+        <ChatTeardropDots 
+          color={theme.colors.text_on_brand_color}
+          size={24}
+          weight="bold"
+        />
+      </TouchableOpacity>
+    </>
   );
 }
--- src/components/Widget/styles.ts
+++ src/components/Widget/styles.ts
@@ -1,7 +1,21 @@
 import { StyleSheet } from 'react-native';
 
+import { getBottomSpace } from'react-native-iphone-x-helper';
+
+import { theme } from '../../theme';
+
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  button: {
+    backgroundColor: theme.colors.brand,
+    borderRadius: 24,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: getBottomSpace() + 16,
+    right: 16,
+
+    height: 48,
+    width: 48,
   }
 });
