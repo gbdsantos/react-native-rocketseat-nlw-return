35b94231b45d82bf762fa919663de7e99f76cc22
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-05-06 00:46:42 -0300

    :sparkles: feat(mobile): create form component

---
+++ src/components/Form/styles.ts
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    paddingHorizontal: 24,
+  },
+
+  header: {
+    flexDirection: 'row',
+    marginVertical: 16,
+  },
+
+  titleContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingRight: 24,
+  },
+
+  image: {
+    height: 24,
+    width: 24,
+    marginRight: 8,
+  },
+
+  titleText: {
+    color: theme.colors.text_primary,
+    fontFamily: theme.fonts.medium,
+    fontSize: 20,
+  },
+
+  input: {
+    borderColor: theme.colors.stroke,
+    borderRadius: 4,
+    borderWidth: 1,
+    color: theme.colors.text_primary,
+    fontFamily: theme.fonts.regular,
+    height: 112,
+    marginBottom: 8,
+    padding: 12,
+  }
+});
--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -7,11 +7,14 @@ import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import { ChatTeardropDots } from 'phosphor-react-native';
 
+import { Form } from '../Form';
 import { Options } from '../Options';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
+import { feedbackTypes } from '../../utils/feedbackTypes';
 
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget(){
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -39,7 +42,9 @@ function Widget(){
         ref={bottomSheetRef} 
         snapPoints={[1, 280]}      
       >
-        <Options />
+        <Form 
+          feedbackType="BUG"
+        />
       </BottomSheet>
     </>
   );
