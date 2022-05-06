de4b39ad4f66a698b8bec5ccea8ecf3eb9ca3aa8
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-05-05 21:56:17 -0300

    :sparkles: feat(mobile): create copyright component

---
+++ src/components/Copyright/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+import { styles } from './styles';
+
+export function Copyright(){
+  return (
+    <View>
+      <Text style={styles.text}>
+        Feito com ♥ por Guilherme Bezerra
+      </Text>
+    </View>
+  );
+}
